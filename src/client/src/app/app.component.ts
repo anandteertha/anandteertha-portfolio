024e77ac8d688ebdc0285560a633f630907d93ee
Anandteertha Rao <raoanandteertha@gmail.com>
2025-06-04 17:45:02 +0530

    STORY-1 Add about component

--- src/client/src/app/app.component.ts
+++ src/client/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BackgroundComponent } from "./modules/background/background.component";
+import { AboutComponent } from "./modules/about/about.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, BackgroundComponent],
+  imports: [RouterOutlet, BackgroundComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
