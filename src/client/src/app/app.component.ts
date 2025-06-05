f8ab5ed776ae6d1cf5be77a2610b7d050ad60a9e
Anandteertha Rao <raoanandteertha@gmail.com>
2025-06-05 09:10:11 +0530

    STORY-1 Add asset, fix scss and add nav bar component

--- src/client/src/app/app.component.ts
+++ src/client/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BackgroundComponent } from "./modules/background/background.component";
 import { AboutComponent } from "./modules/about/about.component";
+import { NavBarComponent } from "./modules/nav-bar/nav-bar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, BackgroundComponent, AboutComponent],
+  imports: [RouterOutlet, BackgroundComponent, AboutComponent, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
