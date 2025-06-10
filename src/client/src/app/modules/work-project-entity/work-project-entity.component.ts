1162ebfdf087d8d3e0399f9b443a1c6cda07bb02
Anandteertha Rao <raoanandteertha@gmail.com>
2025-06-10 09:59:21 +0530

    STORY-2 Add anchor links

--- src/client/src/app/modules/work-project-entity/work-project-entity.component.ts
+++ src/client/src/app/modules/work-project-entity/work-project-entity.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { IContainerTemplate } from '../interfaces/icontainer-template';
 import { CommonModule } from '@angular/common';
+import { camelCase } from 'lodash';
 
 @Component({
   selector: 'app-work-project-entity',
@@ -12,5 +13,9 @@ export class WorkProjectEntityComponent {
 
   @Input() title: string = "";
   @Input() entities: IContainerTemplate[] = [];
+
+  get id(): string {
+    return camelCase(this.title);
+  }
 
 }
