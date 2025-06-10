1b8f843998336718bc9713d6cc5104ff8cbbb187
Anandteertha Rao <raoanandteertha@gmail.com>
2025-06-10 09:25:10 +0530

    STORY-2 Fix generic component css and add project details

--- src/client/src/app/app.component.ts
+++ src/client/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { BackgroundComponent } from "./modules/background/background.component";
 import { AboutComponent } from "./modules/about/about.component";
 import { NavBarComponent } from "./modules/nav-bar/nav-bar.component";
 import { WorkExperienceComponent } from "./modules/work-experience/work-experience.component";
+import { ProjectsComponent } from "./modules/projects/projects.component";
 
 @Component({
   selector: 'app-root',
-  imports: [BackgroundComponent, AboutComponent, NavBarComponent, WorkExperienceComponent],
+  imports: [BackgroundComponent, AboutComponent, NavBarComponent, WorkExperienceComponent, ProjectsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/client/src/app/modules/projects/projects.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { WorkProjectEntityComponent } from "../work-project-entity/work-project-entity.component";
+import { IContainerTemplate } from '../interfaces/icontainer-template';
+
+@Component({
+  selector: 'app-projects',
+  imports: [WorkProjectEntityComponent],
+  templateUrl: './projects.component.html',
+  styleUrl: './projects.component.scss'
+})
+export class ProjectsComponent {
+  title: string = "Projects";
+  projects: IContainerTemplate[] = [
+    {
+      name: 'Faculty Attendance System',
+      description: ['A smart faculty attendance system using facial recognition, geofencing, and secure Node.js backend, enabling one-click attendance with fraud prevention. Published in the Turkish Journal of Computer and Mathematics.'],
+      from: 'January 2021',
+      to: 'May 2021',
+      skills: ['Facial Recognition', 'Python', 'OpenCV', 'Node.js', 'REST APIs', 'Geofencing', 'Haversine Formula', 'Authentication & Authorization', 'Research & Technical Writing'],
+      logoPath: 'assets/faculty-attendance.png',
+      role: ''
+    },
+    {
+      name: 'Crowd Counting',
+      description: ['Built a real-time crowd counting system using YOLO-based human detection and Flask backend', 'Developed custom algorithms to distinguish overlapping individuals and eliminate false positives (e.g., mannequins, posters) from camera footage in malls.'],
+      from: 'January 2021',
+      to: 'April 2021',
+      skills: ['Python', 'YOLO (You Only Look Once)', 'OpenCV', 'Flask', 'Real-time Video Processing', 'Multithreading', 'Data Visualization',],
+      logoPath: 'assets/crowd-counting.png',
+      role: ''
+    },
+    {
+      name: 'Gym Manager Application',
+      description: ['Led development of a full-featured Gym Manager Application supporting user registration, multi-tier subscriptions, seamless payment integration, and real-time trainer-client chat using Socket.io.'],
+      from: 'September 2020',
+      to: 'January 2021',
+      skills: ['NodeJS', 'Socket.io', 'HTML', 'CSS', 'EJS', 'MVC', 'Express JS', 'Stripe', 'MySQL'],
+      logoPath: 'assets/gym-manager-application.png',
+      role: ''
+    },
+  ];
+
+}
